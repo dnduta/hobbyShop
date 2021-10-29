d3f95ccb7f1ebe10c0a267e636e8da1ce8468f6b
Gitau Nduta <dennisgitau@microsoft.com>
2021-10-29 15:19:20 +0300

    Org footer.  Changes to be committed: 	modified:   common/footer-section/footer-section.component.html 	modified:   common/footer-section/footer-section.component.ts

--- src/app/common/footer-section/footer-section.component.ts
+++ src/app/common/footer-section/footer-section.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterSectionComponent implements OnInit {
 
+  currentYear:number = 2022;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.currentYear = new Date().getFullYear();
   }
 
 }
