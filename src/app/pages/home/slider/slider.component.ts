e6ff82a98e29224eb03cec4a8dbb011340e1cf63
Gitau Nduta <dennisgitau@microsoft.com>
2021-10-29 09:38:04 +0300

    Implement slick slider.  Changes to be committed: 	modified:   src/app/pages/home/slider/slider.component.html 	modified:   src/app/pages/home/slider/slider.component.ts

--- src/app/pages/home/slider/slider.component.ts
+++ src/app/pages/home/slider/slider.component.ts
@@ -12,4 +12,35 @@ export class SliderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  slideConfig = {
+    autoplay: true,
+    infinite: true,
+    arrows: true,
+    prevArrow: '<button type=\'button\' class=\'heroSliderArrow prevArrow tf-ion-chevron-left\'></button>',
+    nextArrow: '<button type=\'button\' class=\'heroSliderArrow nextArrow tf-ion-chevron-right\'></button>',
+    dots: true,
+    autoplaySpeed: 7000,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  }
+
+  slickInit(e:any) {
+    console.log('slick initialized');
+    console.log(e);
+  }
+     
+  breakpoint(e:any) {
+    console.log('breakpoint');
+    console.log(e);
+  }
+     
+  afterChange(e:any) {
+    console.log('afterChange');
+    console.log(e);
+  }
+     
+  beforeChange(e:any) {
+    console.log('beforeChange');
+    console.log(e);
+  }
 }
