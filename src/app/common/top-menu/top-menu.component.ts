b3dc22017c884a35c131ccbd8b100616052d87c8
Gitau Nduta <dennisgitau@microsoft.com>
2021-10-29 13:15:19 +0300

    Automate menu links.  Changes to be committed: 	modified:   common/top-menu/top-menu.component.html 	modified:   common/top-menu/top-menu.component.ts

--- src/app/common/top-menu/top-menu.component.ts
+++ src/app/common/top-menu/top-menu.component.ts
@@ -12,4 +12,61 @@ export class TopMenuComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  menuLinks = [
+    {
+      link: '',
+      linkText: 'Home',
+      title: 'PremiumCraft Furniture',
+      children: []
+    },
+    {
+      link: '',
+      linkText: 'Shop',
+      title: 'Shop - PremiumCraft Furniture',
+      children: [
+        {
+          dropdownHeader: 'Office Desks',
+          links: [
+            { link: '', linkText: 'Desk1- awesome', title: 'Awesome - PremiumCraft Furniture' },
+            { link: '', linkText: 'Desk2- awesome', title: 'Awesome - PremiumCraft Furniture' },
+            { link: '', linkText: 'Desk3- awesome', title: 'Awesome - PremiumCraft Furniture' },
+            { link: '', linkText: 'Desk4- awesome', title: 'Awesome - PremiumCraft Furniture' },
+          ]
+        },
+        {
+          dropdownHeader: 'Cofee Tables',
+          links: [
+            { link: '', linkText: 'Tables1- awesome', title: 'Awesome - PremiumCraft Furniture' },
+            { link: '', linkText: 'Tables2- awesome', title: 'Awesome - PremiumCraft Furniture' },
+            { link: '', linkText: 'Tables3- awesome', title: 'Awesome - PremiumCraft Furniture' },
+            { link: '', linkText: 'Tables4- awesome', title: 'Awesome - PremiumCraft Furniture' },
+          ]
+        },
+        {
+          dropdownHeader: 'Side Tables',
+          links: [
+            { link: '', linkText: 'Side1- awesome', title: 'Awesome - PremiumCraft Furniture' },
+            { link: '', linkText: 'Side2- awesome', title: 'Awesome - PremiumCraft Furniture' },
+            { link: '', linkText: 'Side3- awesome', title: 'Awesome - PremiumCraft Furniture' },
+            { link: '', linkText: 'Side4- awesome', title: 'Awesome - PremiumCraft Furniture' },
+          ]
+        }
+      ]
+    },
+    {
+      link: '',
+      linkText: 'About Us',
+      title: 'About Us - PremiumCraft Furniture',
+      children: []
+    },
+    {
+      link: '',
+      linkText: 'Contact Us',
+      title: 'Contact Us - PremiumCraft Furniture',
+      children: []
+    }
+  ];
+
+  submenuSections:number = 4; // bootstrap columns
+
 }
