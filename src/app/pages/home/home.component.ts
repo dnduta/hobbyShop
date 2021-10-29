08e19ab8ec84fc04e27f13478b0d059b9d53a06b
Gitau Nduta <dennisgitau@microsoft.com>
2021-10-29 12:09:17 +0300

    Add page title for home.  Changes to be committed: 	modified:   pages/home/home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  title:Title;
 
+  constructor(_title:Title) {
+    this.title = _title;
+  }
+  
   ngOnInit(): void {
+    this.title.setTitle('PremiumCraft Furniture.');
   }
 
 }
