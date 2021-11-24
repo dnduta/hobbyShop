51bdc479c727d39e69066d77337f4764b8cdd254
Gitau Nduta <dennisgitau@microsoft.com>
2021-11-24 18:34:52 +0300

    More Dummy data

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -17,6 +17,46 @@ export class ShopComponent implements OnInit {
       Photo_url: "../../../assets/images/shop/products/product-1.jpg",
       Name: "Office table",
       Price: "10,000"
+    },    
+    {
+      Photo_url: "../../../assets/images/shop/products/product-2.jpg",
+      Name: "Mani Bed",
+      Price: "10,000"
+    },
+    {
+      Photo_url: "../../../assets/images/shop/products/product-3.jpg",
+      Name: "Office table",
+      Price: "10,000"
+    },    
+    {
+      Photo_url: "../../../assets/images/shop/products/product-4.jpg",
+      Name: "Mani Bed",
+      Price: "10,000"
+    },
+    {
+      Photo_url: "../../../assets/images/shop/products/product-5.jpg",
+      Name: "Office table",
+      Price: "10,000"
+    },    
+    {
+      Photo_url: "../../../assets/images/shop/products/product-6.jpg",
+      Name: "Mani Bed",
+      Price: "10,000"
+    },
+    {
+      Photo_url: "../../../assets/images/shop/products/product-7.jpg",
+      Name: "Office table",
+      Price: "10,000"
+    },    
+    {
+      Photo_url: "../../../assets/images/shop/products/product-8.jpg",
+      Name: "Mani Bed",
+      Price: "10,000"
+    },
+    {
+      Photo_url: "../../../assets/images/shop/products/product-9.jpg",
+      Name: "Office table",
+      Price: "10,000"
     }
   ];
 
