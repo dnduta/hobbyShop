439a2ca244ffefe1f2b7422c70c51c944754f88e
Gitau Nduta <dennisgitau@microsoft.com>
2021-11-24 20:45:34 +0300

    Update home product catalogue.  Changes to be committed: 	modified:   src/app/pages/home/home-top-products/home-top-products.component.html 	modified:   src/app/pages/home/home-top-products/home-top-products.component.ts

--- src/app/pages/home/home-top-products/home-top-products.component.ts
+++ src/app/pages/home/home-top-products/home-top-products.component.ts
@@ -12,4 +12,22 @@ export class HomeTopProductsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  items = [
+    {
+      Photo_url: "../../../assets/images/shop/products/product-1.jpg",
+      Name: "Office table",
+      Price: "10,000"
+    },    
+    {
+      Photo_url: "../../../assets/images/shop/products/product-2.jpg",
+      Name: "Mani Bed",
+      Price: "10,000"
+    },
+    {
+      Photo_url: "../../../assets/images/shop/products/product-3.jpg",
+      Name: "Office table",
+      Price: "10,000"
+    }
+  ];
+
 }
