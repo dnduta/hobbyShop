e12b3bf725be4a0b28d7f260eeec5e2ffb22b4ba
Gitau Nduta <dennisgitau@microsoft.com>
2021-11-25 16:36:24 +0300

    Write-up product details page.

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -12,4 +12,18 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  item = {
+    Name: "Office desk",
+    Description: "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum ipsum dicta quod, quia doloremque aut deserunt commodi quis. Totam a consequatur beatae nostrum, earum consequuntur? Eveniet consequatur ipsum dicta recusandae.</p>" +
+                  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, velit, sunt temporibus, nulla accusamus similique sapiente tempora, at atque cumque assumenda minus asperiores est esse sequi dolore magnam. Debitis, explicabo.",
+    product_images: [
+      '../../../assets/images/shop/single-products/product-1.jpg',
+      '../../../assets/images/shop/single-products/product-2.jpg',
+      '../../../assets/images/shop/single-products/product-3.jpg',
+      '../../../assets/images/shop/single-products/product-4.jpg',
+      '../../../assets/images/shop/single-products/product-5.jpg',
+      '../../../assets/images/shop/single-products/product-6.jpg'
+    ]
+  }
+
 }
